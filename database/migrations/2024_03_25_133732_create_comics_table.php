fde140d12cd2d10a399c481f3084ccde94b257c7
jonathanlombardo <jonathanlombardo90@gmail.com>
2024-03-25 15:34:59 +0100

    add migration

--- database/migrations/2024_03_25_133732_create_comics_table.php
+++ database/migrations/2024_03_25_133732_create_comics_table.php
@@ -4,28 +4,34 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('comics', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+return new class extends Migration {
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('comics', function (Blueprint $table) {
+      $table->id();
+      $table->tinyText("title");
+      $table->string("description")->nullable();
+      $table->tinyText("thumb")->nullable();
+      $table->decimal("price", 8, 2);
+      $table->tinyText("series");
+      $table->date("sale_date")->nullable();
+      $table->tinyText("type");
+      $table->timestamps();
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('comics');
-    }
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('comics');
+  }
 };
